45dbc4ef777e257e579256169ac3b0b9c13c0f73
Eric Coupe <118149003+ecoupe@users.noreply.github.com>
2024-10-21 15:59:46 -0400

    Update CoupeEricM1_ProgrammingAssignment_2.py

--- CoupeEricM1_ProgrammingAssignment_2.py
+++ CoupeEricM1_ProgrammingAssignment_2.py
@@ -14,6 +14,7 @@ server_tip = food_charge*0.18
 sales_tax = food_charge*0.07
 total_bill = food_charge+server_tip+sales_tax
 
+# the outputs are formatted to limit float values to 2 decimals
 print(f"The tip of 18% is: {server_tip:,.2f}")
 print(f"The sales tax is: {sales_tax:,.2f}")
 print(f"The total bill is: {total_bill:,.2f}")
