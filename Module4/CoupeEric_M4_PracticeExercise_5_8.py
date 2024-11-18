0e13022433041462c65fea73435f3cd467c19a6a
Eric Coupe <118149003+ecoupe@users.noreply.github.com>
2024-11-18 11:25:05 -0500

    Add files via upload

--- Module4/CoupeEric_M4_PracticeExercise_5_8.py
+++ Module4/CoupeEric_M4_PracticeExercise_5_8.py
@@ -7,16 +7,23 @@ Short Desc:   A program that opens a text file with data and places the
               text in alphabetical order and shows the frequency the
               word occurs.
 """
+concordance = input("Enter the input file name: ")
 
-with open("ConcordanceTest.txt", 'r') as file:
-    text = file.read().split()
+#uses keyword 'with' to automatically close the file when program ends
+with open(concordance, 'r') as file:
+    text = file.read().split() #reads and splits data
 
+#creates a dictionary to store the words
+#for loop iterates through file to populate the dictionary
+#also creates variable that stores word frequency
 word_counts = {}
 for word in text:
     word_counts[word] = word_counts.get(word, 0) + 1
 
+#sort the dictionary with sorted() function
 sorted_words = sorted(word_counts.keys())
 
+#prints the words in the new sorted list along with the frequency
 for word in sorted_words:
     print(f"{word}: {word_counts[word]}")
 
