090f8eb71d55bf344caedfd08a988820da4361cb
Eric Coupe <118149003+ecoupe@users.noreply.github.com>
2024-11-18 10:48:34 -0500

    Add files via upload

--- Module4/CoupeEric_M4_PracticeExercise_5_7.py
+++ Module4/CoupeEric_M4_PracticeExercise_5_7.py
@@ -7,10 +7,17 @@ Short Desc:   A program that opens a text file with data and sorts the
               text in alphabetical order.
 
 """
-with open("text.txt", 'r') as list_of_words:
-    text = list_of_words.read().split()
+file_name = input("Enter the input file name: ")
 
-all_in_order = sorted(set(text))
+# used 'with' keyword to automatically close the file when program ends
+with open(file_name, 'r') as list_of_words:
+    text = list_of_words.read().split() #splits the list into individual
+                                        #words
+
+#set function removes duplicates, list converts it back into a list for
+#the next line to sort
+all_in_order = list(set(text))
+all_in_order.sort()
 
 for word in all_in_order:
     print(word)
